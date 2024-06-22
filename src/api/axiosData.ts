df39cda6c4062a9a504e4acf4fe823f94fa1269a
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2024-06-22 20:23:35 -0300

    feat: atualizando endereco do json

--- src/api/axiosData.ts
+++ src/api/axiosData.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosData = async ()=>{
     try{
-        const response = await axios.get('/data/data.json')
+        const response = await axios.get('https://raw.githubusercontent.com/JoaoMarcelo22/perfil/main/public/data/data.json')
         return response.data;
     }   catch (error){
         throw new Error("Erro ao buscar dados")
