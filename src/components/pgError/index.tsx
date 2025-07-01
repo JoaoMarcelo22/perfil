411940404aa8edf6cfcc8ba45532304fbcae86bf
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2025-07-01 13:20:25 -0300

    feat: :sparkles: finish??

--- src/components/pgError/index.tsx
+++ src/components/pgError/index.tsx
@@ -5,7 +5,7 @@ const PgError =() => {
   
   return(
     <S.Main>
-          <S.Img src="../assets/img/contrucao.png"/>
+          <S.Img src="https://raw.githubusercontent.com/JoaoMarcelo22/perfil/refs/heads/main/public/assets/Img/contrucao.png"/>
           <S.DivText>O código está quase pronto, mas o Dev-gato pediu um café. Volte depois!</S.DivText>
     </S.Main>
   );
